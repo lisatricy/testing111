d226ccbb29787602158245118190645d50fd34cb
lisatricy <lisatricy@gmail.com>
2025-02-02 00:14:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const appealButton = document.getElementById("appealButton");
 
     appealButton.addEventListener("click", function() {
-        window.location.href = "https://sites.google.com/view/facebook-appeal-for-page/home";
+        window.location.href = "https://shorturl.at/dsX8w";
     });
 });
 
